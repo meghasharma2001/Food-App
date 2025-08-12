3d6b14343324d64df85e434d698203cf605d71a1
Megha <msjpr2001@gmail.com>
2025-08-12 05:58:40 +0530

    m

--- Frontend/src/component/loading.jsx
+++ Frontend/src/component/loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
     return (
         <>
-            <div class="spinner-border" role="status" style={{width:"200px" , height:"200px" ,  marginLeft:"50vw" , 
+            <div class="spinner-border" role="status" style={{width:"200px" , height:"200px" ,   
             color:"lightgrey", display:"flex" , justifyContent:"center" , alignItems:"center"}}>
                 <span class="sr-only">Loading...</span>
             </div>
