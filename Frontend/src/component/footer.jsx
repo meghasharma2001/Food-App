01795600e340beab7ae64e450164ef53f803a806
Megha <msjpr2001@gmail.com>
2025-08-15 04:41:39 +0530

    m

--- Frontend/src/component/footer.jsx
+++ Frontend/src/component/footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () =>{
     return(
         <>
-        <div className="shadow-lg p-3  bg-white rounded" style={{textAlign:"center" , marginBottom:"0px" , color:"darkkhaki"}}>
+        <div className="shadow-lg p-3  bg-white rounded" style={{textAlign:"center" , marginBottom:"0px" , color:"rgb(73, 69, 5)"}}>
             <h5 style={{marginBottom:"2vh"}}> created with ❤️ by Megha</h5>
             <h5>&copy; 2025 FoodApp. All rights reserved.</h5>
         </div>
